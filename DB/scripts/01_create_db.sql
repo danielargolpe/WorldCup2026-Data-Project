da1a9e3ef2a4a9cdf3c455c575223315585cf1d0
Daniela Rodriguez Golpe <danielargolpe@gmail.com>
2026-07-12 19:40:34 -0300

    Add global WorldCup database creation script

---
+++ DB/scripts/01_create_db.sql
@@ -0,0 +1,11 @@
+-- Script 01: Database Creation
+-- Location: DB/01_create_db.sql
+-- Description: Creates the global container database for all World Cup editions.
+
+CREATE DATABASE "WorldCup"
+WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE "WorldCup" IS 'Global database containing multi-year World Cup analytics schemas.';
