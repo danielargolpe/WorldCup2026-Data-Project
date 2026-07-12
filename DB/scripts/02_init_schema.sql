8878d794247377670c5bc79b13fcfb444bab70f7
Daniela Rodriguez Golpe <danielargolpe@gmail.com>
2026-07-12 19:44:49 -0300

    Add dedicated schema for 2026 tournament edition

---
+++ DB/scripts/02_init_schema.sql
@@ -0,0 +1,5 @@
+-- Script 02: Schema inicialization (Edition: 2026)
+-- Location: DB/scripts/02_init_schema.sql
+-- Description: Initializes the dedicated schema for the 2026 World Cup.
+
+CREATE SCHEMA IF NOT EXISTS "2026";
